d34f988a3e54591cdb2b5deb1c52e1b48260bb2e
Mattheus Cassundé <mattheus.089@gmail.com>
2018-10-29 23:42:00 -0300

    Adicionando algoritimo de ordenação e busca

---
+++ estrutura-dados/src/main/java/com/cassunde/graduacao/estrutura/Main.java
@@ -0,0 +1,67 @@
+package com.cassunde.graduacao.estrutura;
+
+public class Main {
+	
+	public static void main(String args[]){
+	
+		int[] numbers = {1,234,21,34,21,345,56,6};
+		
+		for( int j = 0; j <= ( numbers.length -1 ); j++ ) {
+			
+			for( int i = 0; i < ( numbers.length -1 ); i++ ) {
+				
+				int actualValue = numbers[i];
+				int nextValue = numbers[i+1];
+				
+				if( actualValue > nextValue ) {
+					
+					int temp = nextValue;
+					numbers[i+1] = actualValue;
+					numbers[i]   = temp;
+				}
+			}		
+		}				
+		
+		//ordenados
+		for( int i = 0; i <= ( numbers.length -1 ); i++ ) {
+			
+			System.out.println(numbers[i]);
+		}
+		
+		System.out.println("=====================================");
+		
+		int inicio  = 0;
+		int fim     = numbers.length;
+		int posicao = 0;
+		boolean deveContinuar = true;
+		int busca = 800;
+		while(deveContinuar) {
+			
+			posicao = (inicio + fim) / 2;
+			
+			if( posicao <= ( numbers.length -1 ) ) {
+				
+				int valorEncontrado = numbers[posicao];
+				
+				if( valorEncontrado != busca ) {
+					
+					if( valorEncontrado > busca ) {
+						
+						fim = posicao - 1;
+					}else {
+						
+						inicio = posicao + 1;
+					}
+				}else {
+					
+					deveContinuar = false;
+					System.out.println("encontrei na posicao: "+ posicao);
+				}				
+			}else {
+				
+				deveContinuar = false;
+				System.out.println("nao encontrei nada");
+			}
+		}
+    }
+}
